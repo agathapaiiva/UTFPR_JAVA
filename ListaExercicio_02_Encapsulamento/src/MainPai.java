18f1b1dbf42c20ca5dedbb41e460a3e50056fb9f
Agatha Paiva <agathahost@gmail.com>
2020-10-31 10:23:24 -0300

    exercicio_02_encapsulamento

---
+++ ListaExercicio_02_Encapsulamento/src/MainPai.java
@@ -0,0 +1,14 @@
+/**
+ * @author: Agatha dos Santos Paiva 
+ */
+
+public class MainPai {
+	public static void main(String[] args) {
+		
+		Pai p1 = new Pai();
+		
+		p1.cadPai("Wilson", 45, 300);
+		p1.impPai();
+		
+	}
+}
