44dfe9fb758cbaac7cad9883a6993dcac867f35f
Agatha Paiva <agathahost@gmail.com>
2020-11-02 12:45:06 -0300

    evolução do código main

--- ListaExercicio_04_Heranca/src/br/com/utfpr/java/main/Main.java
+++ ListaExercicio_04_Heranca/src/br/com/utfpr/java/main/Main.java
@@ -6,6 +6,8 @@ package br.com.utfpr.java.main;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 import br.com.utfpr.java.dto.Carga;
 import br.com.utfpr.java.dto.Passeio;
 
@@ -13,67 +15,104 @@ public class Main {
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		Passeio veiculoDePasseio = new Passeio();
-		Carga veiculoDeCarga = new Carga();
-
+		String placa = "";
+		String marca = "";
+		String modelo = "";
+		String input;
+		float velocMax = 0;
+		int qtdPist = 0;
+		int potencia = 0;
+		int qtdePassageiros = 0;
+		int numeroVeiculos= 5;
+		int escolha = 0;
+		
+		Passeio[] veiculoDePasseio = new Passeio[numeroVeiculos];
+		Carga[] veiculoDeCarga = new Carga[numeroVeiculos];
+		
+		Passeio veiculoPasseio = new Passeio();
+		Carga veiculoCarga = new Carga();
+		
 		Scanner entradaDado = new Scanner(System.in);
 		
-		System.out.println("==================================================");
-		System.out.println("  		Informe o tipo de veículo");
-		System.out.println("  		1 - PASSEIO  |  2 - CARGA");
-		System.out.println("==================================================\n");
+		input = JOptionPane.showInputDialog("Escolha uma opção\n\n"
+				+"1 - Cadastrar 5 veículo do tipo Passeio\n"
+				+"2 - Cadastrar 5 veículo do tipo Carga\n" );
 		
-		int escolha  = entradaDado.nextInt();
-		float velocMax;
-		String veiculoDePasseios, veiculoDeCargas;
-		
-		switch (escolha) {
-		case 1:
-			veiculoDePasseios = entradaDado.nextLine(); 
-			System.out.println("Informar dados do veículo PASSEIO\n");
-		
-			System.out.println("Digite a placa do veículo ");
-			veiculoDePasseio.setPlaca(entradaDado.nextLine());
-			
-			System.out.println("Digite a marca do veículo ");
-			veiculoDePasseio.setMarca(entradaDado.nextLine());
-			
-			System.out.println("Digite a modelo do veículo ");
-			veiculoDePasseio.setModelo(entradaDado.nextLine());
-			
-			System.out.println("Digite a velocidade do veículo em Km/h ");
-			velocMax = entradaDado.nextFloat();
-			System.out.println("Velocidade digita: " + velocMax + " Km/h " + "convertido: " + 
-					veiculoDePasseio.calcVel(velocMax) + " m/h ");
-			
-			System.out.println("Digite a quantidade de passageiros ");
-			veiculoDePasseio.setQtdePassageiros(entradaDado.nextInt());
-			break;
-		case 2:
-			veiculoDeCargas = entradaDado.nextLine(); 
-			System.out.println("Informar dados do veículo CARGA\n");
-			
-			System.out.println("Digite a placa do veículo ");
-			veiculoDePasseio.setPlaca(entradaDado.nextLine());
-			
-			System.out.println("Digite a marca do veículo ");
-			veiculoDePasseio.setMarca(entradaDado.nextLine());
-			
-			System.out.println("Digite a modelo do veículo ");
-			veiculoDePasseio.setModelo(entradaDado.nextLine());
-			
-			System.out.println("Digite a velocidade do veículo em Km/h ");
-			velocMax = entradaDado.nextFloat();
-			System.out.println(velocMax + " Km/h " + "convertido é : " + 
-					veiculoDeCarga.calcVel(velocMax) + " cm/h ");
-			break;
-		default:
-			System.out.println("Número inválido");
-			break;
-		}
-		
-		entradaDado.close();
-			
-	}
+		try {
+			if(input.equals("1")) {
+				
+				for (int i = 0; i < numeroVeiculos; i++) {
+					veiculoDePasseio[i] = new Passeio();
 
+					System.out.println("Digite a placa do veículo de Passeio" + "[cadastro " + (i + 1) + "]");
+					placa = entradaDado.next();
+					veiculoDePasseio[i].setPlaca(placa);
+
+					System.out.println("Digite a marca do veículo de Passeio" + "[cadastro " + (i + 1) + "]");
+					marca = entradaDado.next();
+					veiculoDePasseio[i].setMarca(marca);
+
+					System.out.println("Digite a modelo do veículo de Passeio" + "[cadastro " + (i + 1) + "]");
+					modelo = entradaDado.next();
+					veiculoDePasseio[i].setModelo(modelo);
+					
+					System.out.println("Digite a quantidade de passageiros de Passeio"  + "[cadastro " + (i + 1) + "]");
+					qtdePassageiros = entradaDado.nextInt();
+					veiculoDePasseio[i].setQtdePassageiros(qtdePassageiros);
+
+					System.out.println("Digite a velocidade do veículo em Km/h " + "[cadastro " + (i + 1) + "]");
+					velocMax = entradaDado.nextFloat();
+					System.out.println("Velocidade digita: " + velocMax + " Km/h " + "convertido: " + 
+							veiculoPasseio.calcVel(velocMax) + " m/h \n");
+					veiculoDePasseio[i].setVelocMax(velocMax);
+
+					System.out.println("Digite a quantidade de pistão do motor de Passeio" + "[cadastro " + (i + 1) + "]");
+					qtdPist = entradaDado.nextInt();
+					veiculoDePasseio[i].getMotor().setQtdPist(qtdPist);
+
+					System.out.println("Digite a potência do motor de Passeio" + "[cadastro " + (i + 1) + "]");
+					potencia = entradaDado.nextInt();
+					veiculoDePasseio[i].getMotor().setPotencia(potencia);
+					System.out.println("================================================================\n");
+				}
+			}else if (input.equals("2")) {
+				for (int i = 0; i < numeroVeiculos; i++) {
+					veiculoDeCarga[i] = new Carga();
+
+					System.out.println("Digite a placa do veículo de Carga" + "[cadastro " + (i + 1) + "]");
+					placa = entradaDado.next();
+					veiculoDeCarga[i].setPlaca(placa);
+
+					System.out.println("Digite a marca do veículo de Carga" + "[cadastro " + (i + 1) + "]");
+					marca = entradaDado.next();
+					veiculoDeCarga[i].setMarca(marca);
+
+					System.out.println("Digite a modelo do veículo de Carga" + "[cadastro " + (i + 1) + "]");
+					modelo = entradaDado.next();
+					veiculoDeCarga[i].setModelo(modelo);
+
+					System.out.println("Digite a velocidade do veículo em Km/h " + "[cadastro " + (i + 1) + "]");
+					velocMax = entradaDado.nextFloat();
+					System.out.println("Velocidade digita: " + velocMax + " Km/h " + "convertido: " + 
+							veiculoCarga.calcVel(velocMax) + " m/h \n");
+					veiculoDeCarga[i].setVelocMax(velocMax);
+
+					System.out.println("Digite a quantidade de pistão do motor de Carga" + "[cadastro " + (i + 1) + "]");
+					qtdPist = entradaDado.nextInt();
+					veiculoDeCarga[i].getMotor().setQtdPist(qtdPist);
+
+					System.out.println("Digite a potência do motor de Carga" + "[cadastro " + (i + 1) + "]");
+					potencia = entradaDado.nextInt();
+					veiculoDeCarga[i].getMotor().setPotencia(potencia);
+					System.out.println("================================================================\n");
+				
+				}
+			}
+			entradaDado.close();
+			
+		} catch (Exception e) {
+			System.out.println("Fechando o programa");
+		}
+
+	}
 }
